02ac8eb74fec9789caf7eb580112d3185faf337a
pandax381 <pandax381@gmail.com>
2015-07-27 18:46:07 +0900

    Changed the log messages format

--- src/common.h
+++ src/common.h
@@ -62,9 +62,11 @@ const typeof( ((type *)0)->member ) *__mptr = (ptr); \
 extern int __debug;
 
 #define __print(type, fmt, ...) \
-    fprintf(stderr, "[%s] " fmt " (%s:%d)\n", type, ##__VA_ARGS__, __FILE__, __LINE__);
+    fprintf(stderr, "%s: " fmt " (%s:%d)\n", type, ##__VA_ARGS__, __FILE__, __LINE__);
 #define error_print(...) \
     __print("error", __VA_ARGS__);
+#define warning_print(...) \
+    __print("warning", __VA_ARGS__);
 #define debug_print(...) \
     if (__debug) \
         __print("degug", __VA_ARGS__);
